f1c983c8fcc9a72f1fe2417326f13b857c456e5a
Sara Harvey <saraharveynyc@gmail.com>
2018-09-15 16:06:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ sentence = "Hello, there, and how are you?"
 
 def reverse_each_word(sentence)
 new_array = []
-new_array << sentence.split.map(&:reverse!).each do |word|
+new_array << sentence.split.collect(&:reverse!).each do |word|
 end
 new_array.join(" ")
 end
